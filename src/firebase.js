498557bae6fee44d6ade869e7a810ae979627d44
Khadeejathul Humaira .M.S <55457122+KhadeejathulHumaira@users.noreply.github.com>
2021-02-26 00:27:45 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAIiMg6yyeRM4HiJXql78TgTyF_lnoxGRQ",
+    authDomain: "slack-clone-5f344.firebaseapp.com",
+    projectId: "slack-clone-5f344",
+    storageBucket: "slack-clone-5f344.appspot.com",
+    messagingSenderId: "154215061950",
+    appId: "1:154215061950:web:0cf1e4f7969f67aa6e01ef"
+  };
+
+  const firebaseApp=firebase.initializeApp(firebaseConfig);
+  const db= firebaseApp.firestore();
+
+
+  export default db;
